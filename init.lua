51541ab9f3f2c962af991404ba2af7c4b39c519f
dungatoro <dungatoro@arch.localdomain>
2023-12-23 19:48:20 +0000

    update

--- init.lua
+++ init.lua
@@ -40,7 +40,8 @@ require('lazy').setup({
       -- Snippet Engine & its associated nvim-cmp source
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
--- Adds LSP completion capabilities
+
+      -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
 
