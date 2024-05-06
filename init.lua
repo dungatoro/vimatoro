777eb79ecdd9ec7c3e9c9438d24fa212dab59899
dung <noajbarker@icloud.com>
2024-05-06 16:09:27 +0100

    Start of a simpler config

--- init.lua
+++ init.lua
@@ -1,383 +1,124 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- Set leader to <Space>
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Install `lazy.nvim` plugin manager
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+-- Set basic neovim options
+vim.opt.number = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+
+-- Load [lazy.nvim](https://github.com/folke/lazy.nvim)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", 
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
-
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-      'aznhe21/actions-preview.nvim',
-    },
-  },
-
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-      'kdheepak/cmp-latex-symbols',
-    },
-  },
-
-  {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-
-  {
+-- Install packages
+require("lazy").setup({
+    -- Theme
     'navarasu/onedark.nvim',
-    priority = 1000,
-  },
 
-  {
-    'nvim-lualine/lualine.nvim',
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      }
-    }
-  },
+    -- LSP base
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
 
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {},
-  },
+	-- LSP management
+	'williamboman/mason.nvim',
+	'williamboman/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
 
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end
-      }
-    }
-  },
+	-- Code actions
+	"aznhe21/actions-preview.nvim",
 
-  'nvim-tree/nvim-web-devicons',
-  'stevearc/oil.nvim',
+	-- Telescope (mainly for UI)
+	'nvim-lua/plenary.nvim',
+	'nvim-telescope/telescope.nvim',
 
-  -- gc to comment highlighted text
-  'numToStr/Comment.nvim',
+	-- Treesitter syntax parser
+	'nvim-treesitter/nvim-treesitter',
 
-  'mg979/vim-visual-multi',
-
-  {'akinsho/toggleterm.nvim', version = "*", opts = {
-    shade_terminals = false,
-    }
-  },
-
-  'jose-elias-alvarez/null-ls.nvim',
-  'semanticart/ruby-code-actions.nvim',
-  'mbbill/undotree',
+	-- Edit files in a vim buffer
+	'stevearc/oil.nvim',
 })
 
-require('onedark').setup {
-  style = 'darker', -- dark darker cool deep warm warmer light
-
-  -- You can configure multiple style with comma separated,
-  -- For e.g., keywords = 'italic,bold'
-  code_style = {
-    comments = 'italic',
-    keywords = 'none',
-    functions = 'none',
-    strings = 'none',
-    variables = 'none'
-  },
-
-  lualine = {
-    transparent = false, -- lualine center bar transparency
-  },
-
-  colors = {},     -- Override default colors
-  highlights = {}, -- Override highlight groups
-
-  diagnostics = {
-    darker = true,     -- darker colors for diagnostic
-    undercurl = true,  -- use undercurl instead of underline for diagnostics
-    background = true, -- use background color for virtual text
-  },
-}
+-- Set theme
+require('onedark').setup { style = 'darker' }
 require('onedark').load()
 
-vim.o.termguicolors = true -- full colours
+local lsp_zero = require('lsp-zero')
+lsp_zero.on_attach(function(client, bufnr)
+    -- `:help lsp-zero-keybindings` for the available actions
+    lsp_zero.default_keymaps({buffer = bufnr})
+end)
 
-vim.wo.number = true
-vim.o.colorcolumn = "81"
-vim.o.signcolumn = "number"
-vim.o.breakindent = true
-vim.o.wrap = false
-vim.o.scrolloff = 8
-
--- tabs
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.hlsearch = false
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
-vim.o.undofile = true
-vim.o.swapfile = false
-
-vim.keymap.set('n', '<leader>uh', vim.cmd.UndotreeToggle)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      }
-    }
-  }
-}
-
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
-local function find_git_root()
-  -- Use the current buffer's path as the starting point for the git search
-  local current_file = vim.api.nvim_buf_get_name(0)
-  local current_dir
-  local cwd = vim.fn.getcwd()
-  -- If the buffer is not associated with a file, return nil
-  if current_file == '' then
-    current_dir = cwd
-  else
-    -- Extract the directory from the current file's path
-    current_dir = vim.fn.fnamemodify(current_file, ':h')
-  end
-
-  -- Find the Git root directory from the current file's path
-  local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
-  if vim.v.shell_error ~= 0 then
-    print 'Not a git repository. Searching on current working directory'
-    return cwd
-  end
-  return git_root
-end
-
--- Custom live_grep function to search in git root
-local function live_grep_git_root()
-  local git_root = find_git_root()
-  if git_root then
-    require('telescope.builtin').live_grep {
-      search_dirs = { git_root },
-    }
-  end
-end
-
-vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
-  { desc = '[?] Find recently opened files' })
-
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
-vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
-    highlight = { enable = true },
-    indent = { enable = true },
-  }
-end, 0)
-
--- This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-  -- We create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = 'LSP: ' .. desc
-    end
-
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-  nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-end
-
-require('mason').setup()
-require('mason-lspconfig').setup()
-
--- Enable the following language servers
-local servers = {
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      diagnostics = { disable = { 'missing-fields' } },
-    }
-  },
-
-}
-
-local mason_lspconfig = require 'mason-lspconfig'
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
-}
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'latex_symbols'},
-  },
-  mapping = { 
-    ['<CR>'] = cmp.mapping.confirm {select = false},
-    ['<C-Space>'] = cmp.mapping.complete {},
-
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
     end,
-  },
-}
-
-require("actions-preview").setup {
-  diff = { ctxlen = 3, },
-  backend = { "telescope" },
-
-  telescope = vim.tbl_extend(
-    "force",
-    require("telescope.themes").get_dropdown(),
-    {
-      make_value = nil,
-      make_make_display = nil,
-    }
-  )
-}
-
-vim.keymap.set({ "v", "n" }, "*", require("actions-preview").code_actions)
-
-local null_ls = require("null-ls")
-local ruby_code_actions = require("ruby-code-actions")
-
-local sources = {
-  null_ls.builtins.formatting.rubocop,
-  null_ls.builtins.diagnostics.rubocop,
-  ruby_code_actions.insert_frozen_string_literal,
-  ruby_code_actions.autocorrect_with_rubocop,
-}
-
-null_ls.setup({ sources = sources })
-
-vim.keymap.set("n", "<leader>ne", [[:e <C-r><C-w>.md <CR>]])
-
-require("oil").setup({
-  view_options = {
-    show_hidden = true,
-    is_hidden_file = function(name, bufnr)
-      return vim.startswith(name, ".")
-    end,
-    is_always_hidden = function(name, bufnr)
-      return false
-    end,
-    sort = {
-      { "type", "asc" },
-      { "name", "asc" },
-    },
   },
 })
 
+require("actions-preview").setup()
+vim.keymap.set({ "v", "n" }, "*", require("actions-preview").code_actions)
+
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+	}
+})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-vim.keymap.set("n", "<leader>ut", [[:!urynus tangle % <CR>]])
-vim.keymap.set("n", "<leader>us", [[:!urynus snip % <C-r><C-w> <CR>]])
-vim.keymap.set("n", "<leader>ui", [[:!urynus init]])
+-- Execute Selected Script
+function execute_script(selection)
+end
 
-vim.keymap.set("n", "<c-/>", "<cmd>ToggleTerm<CR>")
-vim.keymap.set("t", "<c-/>", "<cmd>ToggleTerm<CR>")
+-- List Shell Scripts
+function list_shell_scripts()
+    require('telescope.builtin').find_files({
+        prompt_title = 'Scripts',
+        cwd = '~/.config/nvim/scripts',
+        layout_strategy = 'horizontal',
+        layout_config = {
+            width = 0.6,
+            height = 0.6,
+        },
+        attach_mappings = function(_, map)
+            -- Execute selected script on Enter
+            map('i', '<CR>', function(prompt_bufnr)
+                local selection = require('telescope.actions.state').get_selected_entry(prompt_bufnr)
+                require('telescope.actions').close(prompt_bufnr)
+    			if selection then
+    			    local script_path = selection.path
+    			    vim.fn.jobstart(script_path, {
+    			        on_exit = function(_, _, _)
+    			            print('Script executed successfully')
+    			        end,
+    			        on_stderr = function(_, data, _)
+    			            print('Error:', data)
+    			        end,
+    			    })
+    			end
+            end)
+            return true
+        end,
+    })
+end
 
+-- Key Mapping to List Scripts
+vim.api.nvim_set_keymap('n', '<leader>p', ':lua list_shell_scripts()<CR>', { noremap = true, silent = true })
